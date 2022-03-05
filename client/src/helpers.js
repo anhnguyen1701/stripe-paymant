f037b102ae331dc0ae6befba13ef9e6c8b59fb88
anh nguyen <nguyenlamanh1701@gmail.com>
2022-03-05 19:05:52 +0700

    react add checkout session

---
+++ client/src/helpers.js
@@ -0,0 +1,17 @@
+const API = 'http://localhost:3333';
+
+// hepler function to fetch data from API
+
+export async function fetchFromAPI(endpointURL, opts) {
+  const { method, body } = { method: 'POST', body: null, ...opts };
+
+  const res = await fetch(`${API}/${endpointURL}`, {
+    method,
+    ...(body && { body: JSON.stringify(body) }),
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  });
+
+  return res.json();
+}
